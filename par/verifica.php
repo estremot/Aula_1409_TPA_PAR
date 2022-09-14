3d5d9227d71357f972c8690ddeee98a1e3d6c05f
Marcos Antonio Estremote <52921427+estremot@users.noreply.github.com>
2022-09-14 15:15:30 -0300

    Projeto WEB
    Projeto WEB PHP que verifica se o número é par ou impar.

---
+++ par/verifica.php
@@ -0,0 +1,39 @@
+<?php
+    $n1 = $_GET['num'];
+
+    if($n1 % 2 == 0){
+        $resposta = 'PAR';
+    }
+    else{
+        $resposta = 'impar';
+    }
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <title>Resposta</title>
+</head>
+<body>
+    <h1>Resposta:</h1>
+    <?php if($n1 % 2 == 0){
+    ?>
+    <p class="alert alert-primary">
+        O valor <?php echo($n1); ?> É <?php echo($resposta); ?>
+    </p>
+    <?php
+    }
+    else{
+    ?>
+     <p class="alert alert-danger">
+        O valor <?php echo($n1); ?> É <?php echo($resposta); ?>
+    </p>
+    <?php
+    }
+    ?>
+</body>
+</html>
